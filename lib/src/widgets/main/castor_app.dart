127b81dbbd226e9b61dd083ae0af46bd5e69b403
erlage <erlage.inc@gmail.com>
2022-02-25 23:18:40 +0530

    Add: Property `id` to CastorApp

--- lib/src/widgets/main/castor_app.dart
+++ lib/src/widgets/main/castor_app.dart
@@ -3,9 +3,11 @@ import '/src/widgets/main/app_widget.dart';
 
 class CastorApp extends AppWidget<CastorApp> {
   CastorApp({
+    String? id,
     required Widget child,
     required String targetId,
   }) : super(
+          id: id,
           child: child,
           targetId: targetId,
         );
