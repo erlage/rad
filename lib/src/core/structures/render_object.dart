31f859803215fc0f3f1072c6a2014dc69994f162
erlage <erlage.inc@gmail.com>
2022-02-25 22:54:17 +0530

    Add: Type getter through Type param

--- lib/src/core/structures/render_object.dart
+++ lib/src/core/structures/render_object.dart
@@ -3,21 +3,19 @@ import '/src/core/enums.dart';
 import '/src/core/structures/build_context.dart';
 import '/src/core/structures/widget_object.dart';
 
-abstract class RenderObject {
+abstract class RenderObject<T> {
   late final BuildContext context;
   final BuildableContext buildableContext;
-  final String widgetType;
   final DomTag domTag;
 
   RenderObject({
     required this.buildableContext,
-    required this.widgetType,
     required this.domTag,
   }) {
     context = BuildContext(
       id: buildableContext.id ?? Framework.generateId(),
       parentId: buildableContext.parentId,
-      widgetType: widgetType,
+      widgetType: T.toString(),
       widgetDomTag: domTag,
     );
   }
