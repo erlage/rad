4f9516dedd19bfe0813bba8dbad6836c1dd0638d
erlage <erlage.inc@gmail.com>
2022-02-26 23:34:32 +0530

    Clean: Unnecessary type annots

--- lib/src/widgets/layout/container.dart
+++ lib/src/widgets/layout/container.dart
@@ -3,13 +3,12 @@ import 'package:trad/src/core/classes/painter.dart';
 import 'package:trad/src/core/structures/widget.dart';
 import 'package:trad/src/core/structures/render_object.dart';
 import 'package:trad/src/core/structures/build_context.dart';
-import 'package:trad/src/core/structures/widget_object.dart';
 
 class Container extends Widget {
   final String? key;
-  final String? style;
 
   final Widget child;
+  final String? style;
 
   const Container({
     this.key,
@@ -18,10 +17,10 @@ class Container extends Widget {
   });
 
   @override
-  builder(BuildableContext context) {
+  builder(context) {
     return ContainerRenderObject(
       child: child,
-      style: style,
+      style: style ?? '',
       buildableContext: context.mergeKey(key),
     );
   }
@@ -29,21 +28,23 @@ class Container extends Widget {
 
 class ContainerRenderObject extends RenderObject<Container> {
   final Widget child;
-  final String? style;
+  final String style;
+
+  final BuildableContext buildableContext;
 
   ContainerRenderObject({
     required this.child,
     required this.style,
-    required BuildableContext buildableContext,
+    required this.buildableContext,
   }) : super(
-          buildableContext: buildableContext,
           domTag: DomTag.span,
+          buildableContext: buildableContext,
         );
 
   @override
-  render(WidgetObject widgetObject) {
-    if (null != style) {
-      widgetObject.htmlElement.className = style!; // (!) https://dart.dev/tools/non-promotion-reasons
+  render(widgetObject) {
+    if (style.isNotEmpty) {
+      widgetObject.htmlElement.className = style;
     }
 
     Painter(widgetObject).renderSingleWidget(child);
