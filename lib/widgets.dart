7314f9eb98109ed248a7e1521163a0624620dff9
erlage <erlage.inc@gmail.com>
2022-02-26 11:41:47 +0530

    Change: Split widgets library

---
+++ lib/widgets.dart
@@ -0,0 +1,4 @@
+export 'src/widgets/main/tard_app.dart';
+export 'src/widgets/elements/text.dart' show Text;
+export 'src/widgets/layout/container.dart' show Container;
+export 'src/widgets/misc/gesture_detector.dart' show GestureDetector, HitTestBehavior;
