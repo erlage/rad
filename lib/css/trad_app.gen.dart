13c5ae664fe1294b4b830a61f42665bd2cb352c1
erlage <erlage.inc@gmail.com>
2022-02-26 22:36:36 +0530

    Add: Default CSS styles

---
+++ lib/css/trad_app.gen.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: constant_identifier_names
+
+// auto-generated. please don't edit this file
+
+const GEN_STYLES_TRAD_APP_CSS = ""
+    " /** "
+    "    * app "
+    "    */ "
+    "  "
+    " [data-wtype=\"TradApp\"] { "
+    "     width: 100%; "
+    "     height: 100%; "
+    "     overflow: hidden; "
+    " } ";
