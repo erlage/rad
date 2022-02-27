45b1e705daf8f1c90524a0f234e0a2d6ff75994b
erlage <erlage.inc@gmail.com>
2022-02-27 14:41:14 +0530

    Add: OverlayEntry widget

---
+++ lib/src/widgets/layout/overlay_entry.dart
@@ -0,0 +1,55 @@
+import 'package:trad/src/core/framework.dart';
+import 'package:trad/src/core/enums.dart';
+import 'package:trad/src/core/structures/widget.dart';
+import 'package:trad/src/core/objects/render_object.dart';
+import 'package:trad/src/core/structures/buildable_context.dart';
+
+class OverlayEntry extends Widget {
+  final String? key;
+
+  final Widget child;
+  final String? style;
+
+  const OverlayEntry({
+    this.key,
+    this.style,
+    required this.child,
+  });
+
+  @override
+  builder(context) {
+    return OverlayEntryRenderObject(
+      child: child,
+      style: style ?? '',
+      buildableContext: context.mergeKey(key),
+    );
+  }
+}
+
+class OverlayEntryRenderObject extends RenderObject<OverlayEntry> {
+  final Widget child;
+  final String style;
+
+  final BuildableContext buildableContext;
+
+  OverlayEntryRenderObject({
+    required this.child,
+    required this.style,
+    required this.buildableContext,
+  }) : super(
+          domTag: DomTag.div,
+          buildableContext: buildableContext,
+        );
+
+  @override
+  render(widgetObject) {
+    if (style.isNotEmpty) {
+      widgetObject.htmlElement.className = style;
+    }
+
+    Framework.renderSingleChildWidget(
+      context: context,
+      widget: child,
+    );
+  }
+}
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -4,6 +4,7 @@ export 'src/widgets/layout/container.dart' show Container;
 export 'src/widgets/layout/stack.dart' show Stack;
 export 'src/widgets/layout/align.dart' show Align;
 export 'src/widgets/layout/positioned.dart' show Positioned;
+export 'src/widgets/layout/overlay_entry.dart' show OverlayEntry;
 export 'src/widgets/misc/gesture_detector.dart' show GestureDetector;
 
 export 'package:trad/src/core/enums.dart' show Alignment, HitTestBehavior;
