2e0fdb82a31ae37768bae91158434ca3e76969dc
erlage <erlage.inc@gmail.com>
2022-02-27 17:13:09 +0530

    Add: OverlayState class for managing Overlay state

--- lib/src/widgets/layout/overlay/overlay.dart
+++ lib/src/widgets/layout/overlay/overlay.dart
@@ -1,10 +1,26 @@
+import 'package:rad/rad.dart';
 import 'package:rad/src/core/framework.dart';
 import 'package:rad/src/core/enums.dart';
-import 'package:rad/src/core/structures/widget.dart';
 import 'package:rad/src/core/objects/render_object.dart';
 import 'package:rad/src/core/structures/buildable_context.dart';
 import 'package:rad/src/widgets/layout/overlay/overlay_entry.dart';
+import 'package:rad/src/widgets/layout/overlay/overlay_state.dart';
 
+/// A stack of entries that can be managed independently.
+///
+/// Overlays let independent child widgets "float" visual elements on top of
+/// other widgets by inserting them into the overlay's stack. The overlay lets
+/// each of these widgets manage their participation in the overlay using
+/// [OverlayEntry] objects.
+///
+/// The [Overlay] widget uses a custom stack implementation, which is very
+/// similar to the [Stack] widget but it provides more low-level controls.
+///
+/// See also:
+///
+///  * [OverlayEntry], the class that is used for describing the overlay entries.
+///  * [OverlayState], which is used to insert the entries into the overlay.
+///  * [Stack], which allows directly displaying a stack of widgets.
 class Overlay extends Widget {
   final String? key;
 
@@ -17,6 +33,23 @@ class Overlay extends Widget {
     this.style,
     required this.initialEntries,
   });
+
+  /// The state from the closest instance of this class that encloses the given context.
+  ///
+  /// Typical usage is as follows:
+  ///
+  /// ```dart
+  /// OverlayState overlay = Overlay.of(context);
+  /// ```
+  static OverlayState of(BuildContext context) {
+    var widgetObject = Framework.findAncestorOfType<Overlay>(context);
+
+    if (null == widgetObject) {
+      throw "Overlay.of(context) called with the context that doesn't contains Overylay";
+    }
+
+    return OverlayState(widgetObject);
+  }
 
   @override
   builder(context) {
---
+++ lib/src/widgets/layout/overlay/overlay_state.dart
@@ -0,0 +1,25 @@
+import 'package:rad/src/core/framework.dart';
+import 'package:rad/src/core/objects/widget_object.dart';
+import 'package:rad/src/widgets/layout/overlay/overlay_entry.dart';
+
+class OverlayState {
+  final WidgetObject widgetObject;
+
+  OverlayState(this.widgetObject);
+
+  insert(OverlayEntry overlayEntry) {
+    Framework.renderSingleChildWidget(
+      widget: overlayEntry,
+      context: widgetObject.context,
+      append: true,
+    );
+  }
+
+  insertAll(List<OverlayEntry> entries) {
+    Framework.renderMultipleChildWidgets(
+      widgets: entries,
+      context: widgetObject.context,
+      append: true,
+    );
+  }
+}
