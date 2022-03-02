0b44ee9c217cf97060728b87184870412f408525
erlage <erlage.inc@gmail.com>
2022-03-02 11:07:54 +0530

    Change: PaddingSize to Padding

--- lib/src/core/props/padding.dart
+++ lib/src/core/props/padding.dart
@@ -5,7 +5,7 @@ import 'package:rad/src/core/constants.dart';
 
 /// Padding property object.
 ///
-class PaddingSize {
+class Padding {
   PaddingType type;
 
   double top;
@@ -15,7 +15,7 @@ class PaddingSize {
 
   /// Padding space inside an element.
   ///
-  PaddingSize({
+  Padding({
     required this.top,
     required this.bottom,
     required this.left,
@@ -24,7 +24,7 @@ class PaddingSize {
 
   /// Padding with only the given values.
   ///
-  PaddingSize.only({
+  Padding.only({
     this.top = 0.0,
     this.bottom = 0.0,
     this.left = 0.0,
@@ -33,7 +33,7 @@ class PaddingSize {
 
   /// Symmetrical Padding.
   ///
-  PaddingSize.symmetric({
+  Padding.symmetric({
     double vertical = 0.0,
     double horizontal = 0.0,
   })  : type = PaddingType.symmetric,
@@ -44,7 +44,7 @@ class PaddingSize {
 
   /// Same padding space on all four sides
   ///
-  PaddingSize.all(double value)
+  Padding.all(double value)
       : type = PaddingType.allSame,
         top = value,
         bottom = value,
@@ -53,60 +53,60 @@ class PaddingSize {
 
   // application
 
-  /// Apply padding size.
+  /// Apply padding.
   ///
-  /// if [updatedPaddingSize] is not null, it'll do a update.
+  /// if [updatedPadding] is not null, it'll do a update.
   ///
-  void apply(HtmlElement element, [PaddingSize? updatedPaddingSize]) {
-    if (null == updatedPaddingSize) {
-      return _applySize(element, this);
+  void apply(HtmlElement element, [Padding? updatedPadding]) {
+    if (null == updatedPadding) {
+      return _applyPadding(element, this);
     }
 
-    if (_isChanged(updatedPaddingSize)) {
-      _clearSize(element, this);
-      _updateSize(updatedPaddingSize);
-      _applySize(element, this);
+    if (_isChanged(updatedPadding)) {
+      _clearPadding(element, this);
+      _updatePadding(updatedPadding);
+      _applyPadding(element, this);
     }
   }
 
   // internals
 
-  bool _isChanged(PaddingSize paddingSize) {
-    return top != paddingSize.top ||
-        bottom != paddingSize.bottom ||
-        right != paddingSize.right ||
-        left != paddingSize.left ||
-        type != paddingSize.type;
+  bool _isChanged(Padding padding) {
+    return top != padding.top ||
+        bottom != padding.bottom ||
+        right != padding.right ||
+        left != padding.left ||
+        type != padding.type;
   }
 
-  void _updateSize(PaddingSize paddingSize) {
+  void _updatePadding(Padding padding) {
     this
-      ..top = paddingSize.top
-      ..bottom = paddingSize.bottom
-      ..right = paddingSize.right
-      ..left = paddingSize.left
-      ..type = paddingSize.type;
+      ..top = padding.top
+      ..bottom = padding.bottom
+      ..right = padding.right
+      ..left = padding.left
+      ..type = padding.type;
   }
 
   // statics
 
-  static void _applySize(HtmlElement element, PaddingSize size) {
-    switch (size.type) {
+  static void _applyPadding(HtmlElement element, Padding padding) {
+    switch (padding.type) {
       case PaddingType.only:
-        if (size.top != 0.0) {
-          element.style.setProperty(Props.paddingTop, "${size.top}px");
+        if (padding.top != 0.0) {
+          element.style.setProperty(Props.paddingTop, "${padding.top}px");
         }
 
-        if (size.bottom != 0.0) {
-          element.style.setProperty(Props.paddingBottom, "${size.bottom}px");
+        if (padding.bottom != 0.0) {
+          element.style.setProperty(Props.paddingBottom, "${padding.bottom}px");
         }
 
-        if (size.left != 0.0) {
-          element.style.setProperty(Props.paddingLeft, "${size.left}px");
+        if (padding.left != 0.0) {
+          element.style.setProperty(Props.paddingLeft, "${padding.left}px");
         }
 
-        if (size.right != 0.0) {
-          element.style.setProperty(Props.paddingRight, "${size.right}px");
+        if (padding.right != 0.0) {
+          element.style.setProperty(Props.paddingRight, "${padding.right}px");
         }
 
         break;
@@ -116,29 +116,29 @@ class PaddingSize {
       case PaddingType.allDifferent:
         element.style.setProperty(
           Props.padding,
-          "${size.top}px ${size.right}px ${size.bottom}px ${size.left}px",
+          "${padding.top}px ${padding.right}px ${padding.bottom}px ${padding.left}px",
         );
 
         break;
     }
   }
 
-  static void _clearSize(HtmlElement element, PaddingSize size) {
-    switch (size.type) {
+  static void _clearPadding(HtmlElement element, Padding padding) {
+    switch (padding.type) {
       case PaddingType.only:
-        if (size.top != 0.0) {
+        if (padding.top != 0.0) {
           element.style.removeProperty(Props.paddingTop);
         }
 
-        if (size.bottom != 0.0) {
+        if (padding.bottom != 0.0) {
           element.style.removeProperty(Props.paddingBottom);
         }
 
-        if (size.left != 0.0) {
+        if (padding.left != 0.0) {
           element.style.removeProperty(Props.paddingLeft);
         }
 
-        if (size.right != 0.0) {
+        if (padding.right != 0.0) {
           element.style.removeProperty(Props.paddingRight);
         }
 
