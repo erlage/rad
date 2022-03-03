efd7b5aea9f0a6a7bf9111ef542ff0f0e63ec83b
erlage <erlage.inc@gmail.com>
2022-03-03 10:19:36 +0530

    Update: public widgets API

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -4,14 +4,13 @@ export 'src/core/enums.dart' show HitTestBehavior;
 
 // props
 
-export 'package:rad/src/core/props/padding.dart' show Padding;
-export 'package:rad/src/core/props/margin.dart' show Margin;
+export 'src/core/props/padding.dart' show Padding;
+export 'src/core/props/margin.dart' show Margin;
 
-export 'package:rad/src/core/props/alignment.dart' show Alignment;
+export 'src/core/props/alignment.dart' show Alignment;
 
 // widgets
 
-export 'src/widgets/main/rad_app.dart' show RadApp;
 export 'src/widgets/elements/text.dart' show Text;
 export 'src/widgets/layout/container.dart' show Container;
 export 'src/widgets/layout/stack.dart' show Stack;
@@ -20,3 +19,13 @@ export 'src/widgets/layout/positioned.dart' show Positioned;
 export 'src/widgets/layout/overlay/overlay.dart' show Overlay;
 export 'src/widgets/layout/overlay/overlay_entry.dart' show OverlayEntry;
 export 'src/widgets/misc/gesture_detector.dart' show GestureDetector;
+
+// main
+
+export 'src/widgets/main/rad_app.dart' show RadApp;
+
+// navigator
+
+export 'src/widgets/main/navigator/navigator.dart' show Navigator;
+export 'src/widgets/main/navigator/route.dart' show Route;
+export 'src/widgets/main/navigator/navigator_state.dart' show NavigatorState;
