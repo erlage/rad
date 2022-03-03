8ac56664a5cbab9f6d9c7924bc2a413cceb05520
erlage <erlage.inc@gmail.com>
2022-03-03 21:06:07 +0530

    Change: single quotes to double quotes

--- lib/src/core/constants.dart
+++ lib/src/core/constants.dart
@@ -1,34 +1,34 @@
 class System {
-  static const attrType = 'wtype';
-  static const attrClass = 'wclass';
-  static const attrRoute = 'wroute';
+  static const attrType = "wtype";
+  static const attrClass = "wclass";
+  static const attrRoute = "wroute";
 
-  static const typeBigBang = 'BigBang';
-  static const keyNotSet = 'not-set';
+  static const typeBigBang = "BigBang";
+  static const keyNotSet = "not-set";
 
-  static const coreError = 'Framework has gone wild.';
+  static const coreError = "Framework has gone wild.";
 }
 
 class Props {
-  static const width = 'width';
-  static const height = 'height';
+  static const width = "width";
+  static const height = "height";
 
-  static const top = 'top';
-  static const bottom = 'bottom';
-  static const right = 'right';
-  static const left = 'left';
+  static const top = "top";
+  static const bottom = "bottom";
+  static const right = "right";
+  static const left = "left";
 
-  static const padding = 'padding';
-  static const paddingTop = 'padding-top';
-  static const paddingBottom = 'padding-bottom';
-  static const paddingRight = 'padding-right';
-  static const paddingLeft = 'padding-left';
+  static const padding = "padding";
+  static const paddingTop = "padding-top";
+  static const paddingBottom = "padding-bottom";
+  static const paddingRight = "padding-right";
+  static const paddingLeft = "padding-left";
 
-  static const margin = 'margin';
-  static const marginTop = 'margin-top';
-  static const marginBottom = 'margin-bottom';
-  static const marginRight = 'margin-right';
-  static const marginLeft = 'margin-left';
+  static const margin = "margin";
+  static const marginTop = "margin-top";
+  static const marginBottom = "margin-bottom";
+  static const marginRight = "margin-right";
+  static const marginLeft = "margin-left";
 
-  static const transform = 'transform';
+  static const transform = "transform";
 }
