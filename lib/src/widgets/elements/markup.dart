5a4ad992aeeeb945aeb0910293c95d973ede001f
erlage <erlage.inc@gmail.com>
2022-03-05 00:00:59 +0530

    Add: Markup widget

---
+++ lib/src/widgets/elements/markup.dart
@@ -0,0 +1,64 @@
+import 'package:rad/src/core/constants.dart';
+import 'package:rad/src/core/enums.dart';
+import 'package:rad/src/core/objects/render_object.dart';
+import 'package:rad/src/core/objects/build_context.dart';
+import 'package:rad/src/core/classes/abstract/widget.dart';
+
+/// A widget that helps pushing raw contents to the DOM.
+///
+/// This widget doesn't clean or sanitize inputs. This behaviour is
+/// intentional to allow an application add Javascript into the DOM.
+///
+/// Example:
+///
+/// ```dart
+/// Container(
+///   child: Markup('<table> {...} </table>'),
+/// )
+/// ```
+///
+class MarkUp extends Widget {
+  final String? key;
+
+  final String html;
+
+  const MarkUp(
+    this.html, {
+    this.key,
+  });
+
+  @override
+  DomTag get tag => DomTag.span;
+
+  @override
+  String get type => (MarkUp).toString();
+
+  @override
+  String get initialKey => key ?? System.keyNotSet;
+
+  @override
+  createRenderObject(context) {
+    return MarkupRenderObject(html: html, context: context);
+  }
+}
+
+class MarkupRenderObject extends RenderObject {
+  final String html;
+
+  MarkupRenderObject({
+    required this.html,
+    required BuildContext context,
+  }) : super(context);
+
+  @override
+  render(widgetObject) => widgetObject.element.innerHtml = html;
+
+  @override
+  update(widgetObject, updatedRenderObject) {
+    updatedRenderObject as MarkupRenderObject;
+
+    if (html != updatedRenderObject.html) {
+      widgetObject.element.innerHtml = updatedRenderObject.html;
+    }
+  }
+}
