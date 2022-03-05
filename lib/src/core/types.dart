f2d868a8d91081ec9d6467fbff53faba3cd0c365
erlage <erlage.inc@gmail.com>
2022-03-06 00:19:03 +0530

    Add: Missing type annots

--- lib/src/core/types.dart
+++ lib/src/core/types.dart
@@ -4,11 +4,11 @@ import 'package:rad/src/core/classes/abstract/widget.dart';
 import 'package:rad/src/core/objects/build_context.dart';
 import 'package:rad/src/core/objects/widget_object.dart';
 
-typedef OnClickCallback = Function(MouseEvent event);
+typedef OnClickCallback = void Function(MouseEvent event);
 
-typedef OnTapEventCallback = Function(Event event);
+typedef OnTapEventCallback = void Function(Event event);
 
-typedef ElementCallback = Function(HtmlElement element);
+typedef ElementCallback = void Function(HtmlElement element);
 
 typedef WidgetBuilderCallback = Widget Function(BuildContext context);
 
