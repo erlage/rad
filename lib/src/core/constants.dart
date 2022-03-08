f7743a966425e31e400403bf5891beef037565cb
erlage <erlage.inc@gmail.com>
2022-03-08 09:31:24 +0530

    Clean: dead code

--- lib/src/core/constants.dart
+++ lib/src/core/constants.dart
@@ -10,30 +10,3 @@ class System {
   static const coreError = "Framework has gone wild.";
   static const routerError = "Router has gone wild.";
 }
-
-class Props {
-  static const width = "width";
-  static const height = "height";
-
-  static const top = "top";
-  static const bottom = "bottom";
-  static const right = "right";
-  static const left = "left";
-
-  static const margin = "margin";
-  static const padding = "padding";
-
-  static const transform = "transform";
-
-  // flex item related
-
-  static const flex = 'flex';
-
-  // flex box related
-
-  static const gap = 'gap';
-  static const flexWrap = 'flex-wrap';
-  static const alignItems = 'align-items';
-  static const flexDirection = 'flex-direction';
-  static const justifyContent = 'justify-content';
-}
