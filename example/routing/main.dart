27e665da43b2831896f9418c5e61ed8b11f83a14
erlage <erlage.inc@gmail.com>
2022-03-15 21:11:07 +0530

    Re-add: Routing example

---
+++ example/routing/main.dart
@@ -0,0 +1,115 @@
+import 'package:rad/rad.dart';
+
+import 'settings_page.dart';
+
+void main() {
+  App(
+    targetKey: "output",
+    child: RootPage(),
+    debugOptions: DebugOptions(
+      widgetLogs: true,
+      developmentMode: true,
+    ),
+  );
+}
+
+class RootPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  String _activeRoute = "";
+  NavigatorState? _navigatorState;
+
+  @override
+  build(context) {
+    return Division(
+      style: "display: flex; flex-direction: column;",
+      children: [
+        _rootPageTopNav(),
+        Division(
+          children: [
+            _rootPageNavigator(),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _rootPageNavigator() {
+    return Navigator(
+      onInit: _onNavigatorInit,
+      onRouteChange: _onNavigatorRouteChange,
+      routes: [
+        Route(name: 'home', page: HomePage()),
+        Route(name: 'posts', page: PostsPage()),
+        Route(name: 'settings', page: SettingsPage()),
+      ],
+    );
+  }
+
+  Widget _rootPageTopNav() {
+    return Division(
+      classAttribute: "header",
+      style: "width:100%; height:50px;",
+      children: [
+        Division(
+          style:
+              "display: flex; flex-direction: row; gap:20px; justify-content: center;",
+          children: [
+            _headerItem(text: "Home", routeName: "home"),
+            _headerItem(text: "Posts", routeName: "posts"),
+            _headerItem(text: "Settings", routeName: "settings"),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _headerItem({required String routeName, required String text}) {
+    return GestureDetector(
+      onTap: () => _navigatorState?.open(name: routeName),
+      child: Division(
+        style: "width: 100px;padding:15px",
+        classAttribute: _activeRoute == routeName ? "active" : "",
+        children: [
+          Division(
+            style: "margin: 0 auto;",
+            children: [Text(text)],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onNavigatorInit(NavigatorState state) {
+    _navigatorState = state;
+  }
+
+  void _onNavigatorRouteChange(String name) {
+    setState(() {
+      _activeRoute = name;
+    });
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  build(context) {
+    return Division(
+      style: "margin: 0 auto;",
+      innerText: "Home page",
+    );
+  }
+}
+
+class PostsPage extends StatelessWidget {
+  @override
+  build(context) {
+    return Division(
+      style: "margin: 0 auto;",
+      innerText: "Posts page",
+    );
+  }
+}
---
+++ example/routing/settings_page.dart
@@ -0,0 +1,101 @@
+import 'package:rad/rad.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  String _activeRoute = "";
+  NavigatorState? _navigatorState;
+
+  @override
+  build(context) {
+    return Division(
+      style:
+          "display: flex; flex-direction: row; gap:20px; justify-content: center;",
+      children: [
+        _settingsPageVeritcalNav(),
+        Division(
+          style: "flex: 1",
+          children: [_settingsPageNavigator()],
+        ),
+      ],
+    );
+  }
+
+  Widget _settingsPageNavigator() {
+    return Navigator(
+      onInit: _onNavigatorInit,
+      onRouteChange: _onNavigatorRouteChange,
+      routes: [
+        Route(name: 'account', page: Page("Account Settings")),
+        Route(name: 'photos', page: Page("Photos Settings")),
+        Route(name: 'videos', page: Page("Videos Settings")),
+        Route(name: 'personal', page: Page("Personal Settings")),
+      ],
+    );
+  }
+
+  Widget _settingsPageVeritcalNav() {
+    return Division(
+      style: "width:250px; height: 100%;",
+      classAttribute: "vertical-header",
+      children: [
+        Division(
+          style:
+              "display: flex; flex-direction: column; justify-content: flex-start; gap: 10px;",
+          children: [
+            Division(style: "width: 100%; height: 20px;"),
+            _headerItem(text: "Account settings", routeName: "account"),
+            _headerItem(text: "Photos settings", routeName: "photos"),
+            _headerItem(text: "Videos settings", routeName: "videos"),
+            _headerItem(text: "Personal information", routeName: "personal"),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _headerItem({required String routeName, required String text}) {
+    return GestureDetector(
+      onTap: () {
+        _navigatorState?.open(name: routeName);
+      },
+      child: Division(
+        style: "widht: 100%",
+        classAttribute: _activeRoute == routeName ? "active" : "",
+        children: [
+          Division(
+            style: "padding: 15px;",
+            innerText: text,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onNavigatorInit(NavigatorState state) {
+    _navigatorState = state;
+  }
+
+  void _onNavigatorRouteChange(String name) {
+    setState(() {
+      _activeRoute = name;
+    });
+  }
+}
+
+class Page extends StatelessWidget {
+  final String title;
+
+  Page(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Division(
+      style: "margin: 0 auto;",
+      children: [Text(title)],
+    );
+  }
+}
