c6eae074d0136e47865292478521a02b241799b1
erlage <erlage.inc@gmail.com>
2022-04-17 17:26:12 +0530

    Use Task Scheduler to bootstrap App

--- lib/src/core/functions.dart
+++ lib/src/core/functions.dart
@@ -2,9 +2,11 @@ import 'dart:html';
 
 import 'package:rad/src/core/classes/debug.dart';
 import 'package:rad/src/core/classes/framework.dart';
+import 'package:rad/src/core/classes/registry.dart';
 import 'package:rad/src/core/constants.dart';
 import 'package:rad/src/core/objects/build_context.dart';
 import 'package:rad/src/core/objects/debug_options.dart';
+import 'package:rad/src/core/scheduler/tasks/widgets_build_task.dart';
 import 'package:rad/src/widgets/abstract/widget.dart';
 import 'package:rad/src/widgets/utils/common_props.dart';
 
@@ -61,8 +63,16 @@ void startApp({
 
   // Schedule a build task.
 
-  framework.buildChildren(
-    widgets: [app],
-    parentContext: rootContext,
+  // Framework is responsible for resgistering a task schedular for provided
+  // root context when its init() method is called. therefore by this point
+  // we assume that our scheduler for context is all set and ready to go.
+
+  var scheduler = Registry.instance.getTaskScheduler(rootContext);
+
+  scheduler.addTask(
+    WidgetsBuildTask(
+      widgets: [app],
+      parentContext: rootContext,
+    ),
   );
 }
