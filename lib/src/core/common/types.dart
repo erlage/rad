7aef21f7adc1fb2807932db2f4557194327316ea
erlage <erlage.inc@gmail.com>
2022-04-18 11:23:20 +0530

    Update types

--- lib/src/core/common/types.dart
+++ lib/src/core/common/types.dart
@@ -9,11 +9,12 @@ import 'package:rad/src/widgets/abstract/widget.dart';
 import 'package:rad/src/widgets/list_view.dart';
 import 'package:rad/src/widgets/navigator.dart';
 
+typedef EventCallback = void Function(Event event);
+typedef OnTapEventCallback = void Function(Event event);
+
 typedef SchedulerTaskCallback = void Function(SchedulerTask task);
 
 typedef UpdateTypeCallback = bool Function(UpdateType updateType);
-
-typedef OnTapEventCallback = void Function(Event event);
 
 typedef NavigatorRouteChangeCallback = void Function(String name);
 
@@ -21,21 +22,22 @@ typedef NavigatorStateCallback = void Function(NavigatorState state);
 
 typedef WidgetBuilderCallback = Widget Function(BuildContext context);
 
-/// Signature for a function that creates a widget for a given index, e.g., in a
-/// list.
-///
-/// Used by [ListView.builder].
-///
-typedef IndexedWidgetBuilder = Widget Function(BuildContext context, int index);
+typedef ExceptionCallback = void Function(Exception event);
 
-typedef WidgetActionCallback = List<WidgetAction> Function(
-  WidgetObject widgetObject,
-);
+typedef PopStateEventCallback = void Function(PopStateEvent event);
 
 typedef RenderElementCallback = void Function(
   RenderObject renderObject,
   HtmlElement element,
 );
 
-typedef EventCallback = void Function(Event event);
-typedef ExceptionCallback = void Function(Exception event);
+typedef WidgetActionCallback = List<WidgetAction> Function(
+  WidgetObject widgetObject,
+);
+
+/// Signature for a function that creates a widget for a given index, e.g., in a
+/// list.
+///
+/// Used by [ListView.builder].
+///
+typedef IndexedWidgetBuilder = Widget Function(BuildContext context, int index);
