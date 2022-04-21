d376066bb73ee8f6baf007148ac1d080e901c746
erlage <erlage.inc@gmail.com>
2022-04-21 08:10:02 +0530

    Tests: Add for KeyGen.getGlobalKey

--- test/tests/core/services/keygen_test.dart
+++ test/tests/core/services/keygen_test.dart
@@ -8,11 +8,11 @@ import '../../../fixers/test_bed.dart';
 void main() {
   /*
   |--------------------------------------------------------------------------
-  | keyGenService.generateWidgetKey() | Key generator test (a very basic test)
+  | keyGenService.generateGlobalKey() | Key generator test (a very basic test)
   |--------------------------------------------------------------------------
   */
 
-  group('generateWidgetKey() :', () {
+  group('generateGlobalKey() :', () {
     var keyGenService = KeyGen(RT_TestBed.rootContext);
 
     var iterations = 100;
@@ -32,6 +32,46 @@ void main() {
     test('should generate keys that starts with system a identifier', () {
       for (var key in generatedWidgetKeys) {
         expect(key.hasSystemPrefix, equals(true));
+      }
+    });
+  });
+
+  /*
+  |--------------------------------------------------------------------------
+  | keyGenService.generateGlobalKey() | Key generator test (a very basic test)
+  |--------------------------------------------------------------------------
+  */
+
+  group('generateGlobalKey() :', () {
+    var keyGenService = KeyGen(RT_TestBed.rootContext);
+
+    var iterations = 100;
+    var generatedWidgetKeys = <Key>[];
+
+    while (iterations-- > 0) {
+      var key = Key("$iterations");
+
+      generatedWidgetKeys.add(
+        keyGenService.getGlobalKeyUsingKey(
+          key,
+          RT_TestBed.rootContext,
+        ),
+      );
+    }
+
+    test('should generate unique keys', () {
+      expect(
+        generatedWidgetKeys.length,
+        equals({...generatedWidgetKeys}.length),
+      );
+    });
+
+    test('should generate keys that starts with app target id', () {
+      for (var key in generatedWidgetKeys) {
+        expect(
+          key.value.startsWith(RT_TestBed.rootContext.appTargetId),
+          equals(true),
+        );
       }
     });
   });
