ea4126d28806ea6abb8d209ab66d16e3b3df5dc4
erlage <erlage.inc@gmail.com>
2022-04-27 19:26:01 +0530

    Tests: Add basic tests for StatefulWidget

--- test/fixers/test_widget.dart
+++ test/fixers/test_widget.dart
@@ -2,12 +2,7 @@
 
 import 'package:meta/meta.dart';
 
-import 'package:rad/src/core/common/types.dart';
-import 'package:rad/src/core/common/enums.dart';
-import 'package:rad/src/core/common/objects/build_context.dart';
-import 'package:rad/src/core/common/objects/render_object.dart';
-import 'package:rad/src/widgets/abstract/widget.dart';
-import 'package:rad/src/core/common/objects/key.dart';
+import '../test_imports.dart';
 
 /// A widget that allows hooking its internals.
 ///
@@ -207,6 +202,173 @@ class RT_TestWidgetRenderObject extends RenderObject {
   beforeUnMount() {
     if (null != roEventHookBeforeUnMount) {
       roEventHookBeforeUnMount!();
+    }
+  }
+}
+
+/// A StatefulWidget that allows hooking its internals.
+///
+class RT_StatefulTestWidget extends StatefulWidget {
+  final Callback? stateEventInitState;
+  final Callback? stateEventDidChangeDependencies;
+  final Callback? stateEventDidUpdateWidget;
+  final Callback? stateEventBuild;
+  final Callback? stateEventDispose;
+  final Callback? stateEventCreateState;
+
+  final Function(RT_StatefulTestWidget_State state)? stateHookInitState;
+
+  final Function(RT_StatefulTestWidget_State state)? stateHookBuild;
+
+  final Function(
+    RT_StatefulTestWidget_State state,
+  )? stateHookDidChangeDependencies;
+
+  final Function(
+    RT_StatefulTestWidget_State state,
+    Widget old,
+  )? stateHookDidUpdateWidget;
+
+  final String hash;
+
+  const RT_StatefulTestWidget({
+    Key? key,
+    this.stateEventInitState,
+    this.stateEventDidChangeDependencies,
+    this.stateEventDidUpdateWidget,
+    this.stateEventBuild,
+    this.stateEventDispose,
+    this.stateEventCreateState,
+    this.stateHookInitState,
+    this.stateHookBuild,
+    this.stateHookDidUpdateWidget,
+    this.stateHookDidChangeDependencies,
+    String? customHash,
+  })  : hash = customHash ?? 'none',
+        super(key: key);
+
+  @override
+  RT_StatefulTestWidget_State createState() => RT_StatefulTestWidget_State(
+        stateEventCreateState,
+      );
+}
+
+class RT_StatefulTestWidget_State extends State<RT_StatefulTestWidget> {
+  Callback? _stateEventInitState;
+  Callback? _stateEventDidChangeDependencies;
+  Callback? _stateEventDidUpdateWidget;
+  Callback? _stateEventBuild;
+  Callback? _stateEventDispose;
+
+  Function(RT_StatefulTestWidget_State state)? _stateHookInitState;
+
+  Function(RT_StatefulTestWidget_State state)? _stateHookBuild;
+
+  Function(
+    RT_StatefulTestWidget_State state,
+  )? _stateHookDidChangeDependencies;
+
+  Function(
+    RT_StatefulTestWidget_State state,
+    Widget old,
+  )? _stateHookDidUpdateWidget;
+
+  RT_StatefulTestWidget_State(Callback? stateEventCreateState) {
+    if (null != stateEventCreateState) {
+      stateEventCreateState();
+    }
+  }
+
+  @override
+  initState() {
+    bindHooks();
+
+    if (null != _stateEventInitState) {
+      _stateEventInitState!();
+    }
+
+    if (null != _stateHookInitState) {
+      _stateHookInitState!(this);
+    }
+  }
+
+  @override
+  didChangeDependencies() {
+    if (null != _stateEventDidChangeDependencies) {
+      _stateEventDidChangeDependencies!();
+    }
+
+    if (null != _stateHookDidChangeDependencies) {
+      _stateHookDidChangeDependencies!(this);
+    }
+  }
+
+  @override
+  didUpdateWidget(oldWidget) {
+    if (null != _stateEventDidUpdateWidget) {
+      _stateEventDidUpdateWidget!();
+    }
+
+    if (null != _stateHookDidUpdateWidget) {
+      _stateHookDidUpdateWidget!(this, oldWidget);
+    }
+  }
+
+  @override
+  build(context) {
+    if (null != _stateEventBuild) {
+      _stateEventBuild!();
+    }
+
+    if (null != _stateHookBuild) {
+      _stateHookBuild!(this);
+    }
+
+    return Text('hello world');
+  }
+
+  @override
+  dispose() {
+    if (null != _stateEventDispose) {
+      _stateEventDispose!();
+    }
+  }
+
+  void bindHooks() {
+    if (null != widget.stateEventInitState) {
+      _stateEventInitState = widget.stateEventInitState;
+    }
+
+    if (null != widget.stateEventDidChangeDependencies) {
+      _stateEventDidChangeDependencies = widget.stateEventDidChangeDependencies;
+    }
+
+    if (null != widget.stateEventDidUpdateWidget) {
+      _stateEventDidUpdateWidget = widget.stateEventDidUpdateWidget;
+    }
+
+    if (null != widget.stateEventBuild) {
+      _stateEventBuild = widget.stateEventBuild;
+    }
+
+    if (null != widget.stateEventDispose) {
+      _stateEventDispose = widget.stateEventDispose;
+    }
+
+    if (null != widget.stateHookInitState) {
+      _stateHookInitState = widget.stateHookInitState;
+    }
+
+    if (null != widget.stateHookInitState) {
+      _stateHookInitState = widget.stateHookInitState;
+    }
+
+    if (null != widget.stateHookDidUpdateWidget) {
+      _stateHookDidUpdateWidget = widget.stateHookDidUpdateWidget;
+    }
+
+    if (null != widget.stateHookDidChangeDependencies) {
+      _stateHookDidChangeDependencies = widget.stateHookDidChangeDependencies;
     }
   }
 }
---
+++ test/tests/widgets/stateful_widget_test.dart
@@ -0,0 +1,277 @@
+// ignore_for_file: camel_case_types
+
+import '../../test_imports.dart';
+
+void main() {
+  group('State hooks :', () {
+    setUp(() {
+      ServicesRegistry.instance.unRegisterServices(RT_TestBed.rootContext);
+    });
+
+    test('should bind widget before initState', () {
+      startApp(
+        app: RT_StatefulTestWidget(
+          key: GlobalKey('widget'),
+          stateHookInitState: (state) {
+            expect(state.widget.runtimeType, equals(RT_StatefulTestWidget));
+          },
+        ),
+        targetId: RT_TestBed.rootContext.key.value,
+      );
+    });
+
+    test('should bind widget before initState', () {
+      startApp(
+        app: RT_StatefulTestWidget(
+          key: GlobalKey('widget'),
+          stateHookInitState: (state) {
+            expect(state.context.key.value, equals('widget'));
+          },
+        ),
+        targetId: RT_TestBed.rootContext.key.value,
+      );
+    });
+
+    test('should call did change dependencies after initState', () async {
+      var testStack = RT_TestStack();
+
+      startApp(
+        app: RT_StatefulTestWidget(
+          stateEventInitState: () => testStack.push('init state'),
+          stateEventDidChangeDependencies: () => testStack.push(
+            'did change dependencies',
+          ),
+        ),
+        targetId: RT_TestBed.rootContext.key.value,
+      );
+
+      await Future.delayed(Duration.zero, () {
+        expect(testStack.popFromStart(), equals('init state'));
+        expect(testStack.popFromStart(), equals('did change dependencies'));
+        expect(testStack.canPop(), equals(false));
+      });
+    });
+
+    test('should call build after did change dependencies', () async {
+      var testStack = RT_TestStack();
+
+      startApp(
+        app: RT_StatefulTestWidget(
+          stateEventInitState: () => testStack.push('init state'),
+          stateEventBuild: () => testStack.push('build'),
+          stateEventDidChangeDependencies: () => testStack.push(
+            'did change dependencies',
+          ),
+        ),
+        targetId: RT_TestBed.rootContext.key.value,
+      );
+
+      await Future.delayed(Duration.zero, () {
+        expect(testStack.popFromStart(), equals('init state'));
+        expect(testStack.popFromStart(), equals('did change dependencies'));
+        expect(testStack.popFromStart(), equals('build'));
+        expect(testStack.canPop(), equals(false));
+      });
+    });
+
+    test('should defer setState called in initState', () async {
+      var testStack = RT_TestStack();
+      var i = 1;
+
+      startApp(
+        app: RT_StatefulTestWidget(
+          stateHookInitState: (state) {
+            testStack.push('init state');
+
+            // ignore: invalid_use_of_protected_member
+            state.setState(() {
+              testStack.push('set state');
+            });
+          },
+          stateEventBuild: () => testStack.push('build-${i++}'),
+          stateEventDidUpdateWidget: () => testStack.push('did update widget'),
+          stateEventDidChangeDependencies: () => testStack.push(
+            'did change dependencies',
+          ),
+        ),
+        targetId: RT_TestBed.rootContext.key.value,
+      );
+
+      await Future.delayed(Duration.zero, () {
+        expect(testStack.popFromStart(), equals('init state'));
+        expect(testStack.popFromStart(), equals('did change dependencies'));
+        expect(testStack.popFromStart(), equals('build-1'));
+        expect(testStack.popFromStart(), equals('set state'));
+        expect(testStack.popFromStart(), equals('build-2'));
+        expect(testStack.canPop(), equals(false));
+      });
+    });
+
+    test('should not call didUpdateWidget for initial build', () async {
+      var testStack = RT_TestStack();
+
+      startApp(
+        app: RT_StatefulTestWidget(
+          stateEventInitState: () => testStack.push('init state'),
+          stateEventBuild: () => testStack.push('build'),
+          stateEventDidUpdateWidget: () => testStack.push('did update widget'),
+          stateEventDidChangeDependencies: () => testStack.push(
+            'did change dependencies',
+          ),
+        ),
+        targetId: RT_TestBed.rootContext.key.value,
+      );
+
+      await Future.delayed(Duration.zero, () {
+        expect(testStack.popFromStart(), equals('init state'));
+        expect(testStack.popFromStart(), equals('did change dependencies'));
+        expect(testStack.popFromStart(), equals('build'));
+        expect(testStack.canPop(), equals(false));
+      });
+    });
+
+    test(
+      'should call didUpdateWidget with old widget when widget is swapped',
+      () async {
+        var testStack = RT_TestStack();
+        var i = 1;
+
+        var app = createTestApp()..start();
+
+        app.framework.buildChildren(
+          widgets: [
+            RT_StatefulTestWidget(
+              customHash: 'old',
+              stateEventCreateState: () => testStack.push('create state 1a'),
+              stateEventInitState: () => testStack.push('init state 1a'),
+              stateEventBuild: () => testStack.push('build-${i++} 1a'),
+              stateEventDidUpdateWidget: () => testStack.push(
+                'did update widget 1a',
+              ),
+              stateHookDidUpdateWidget: (state, widget) {
+                widget as RT_StatefulTestWidget;
+
+                expect(widget.hash, equals('old'));
+                expect(state.widget.hash, equals('new'));
+              },
+              stateEventDidChangeDependencies: () => testStack.push(
+                'did change dependencies 1a',
+              ),
+            ),
+          ],
+          parentContext: RT_TestBed.rootContext,
+        );
+
+        await Future.delayed(Duration.zero, () {
+          app.framework.updateChildren(
+            widgets: [
+              RT_StatefulTestWidget(
+                customHash: 'new',
+                stateEventCreateState: () => testStack.push('create state 1b'),
+                stateEventInitState: () => testStack.push('init state 1b'),
+                stateEventBuild: () => testStack.push('build-${i++} 1b'),
+                stateEventDidUpdateWidget: () => testStack.push(
+                  'did update widget 1b',
+                ),
+                stateHookDidUpdateWidget: (state, widget) {
+                  throw "should never gets called";
+                },
+                stateEventDidChangeDependencies: () => testStack.push(
+                  'did change dependencies 1b',
+                ),
+              ),
+            ],
+            updateType: UpdateType.undefined,
+            parentContext: RT_TestBed.rootContext,
+          );
+        });
+
+        await Future.delayed(Duration.zero, () {
+          print(testStack.entries.toString());
+
+          expect(testStack.popFromStart(), equals('create state 1a'));
+          expect(testStack.popFromStart(), equals('init state 1a'));
+          expect(
+            testStack.popFromStart(),
+            equals('did change dependencies 1a'),
+          );
+          expect(testStack.popFromStart(), equals('build-1 1a'));
+          expect(testStack.popFromStart(), equals('did update widget 1a'));
+          expect(testStack.popFromStart(), equals('build-2 1a'));
+          expect(testStack.canPop(), equals(false));
+        });
+      },
+    );
+
+    test('should call dispose when widget is removed', () async {
+      var testStack = RT_TestStack();
+
+      var app = createTestApp()..start();
+
+      app.framework.buildChildren(
+        widgets: [
+          RT_StatefulTestWidget(
+            key: GlobalKey('old'),
+            stateEventCreateState: () => testStack.push('create state 1a'),
+            stateEventInitState: () => testStack.push('init state 1a'),
+            stateEventBuild: () => testStack.push('build 1a'),
+            stateEventDidUpdateWidget: () => testStack.push(
+              'did update widget 1a',
+            ),
+            stateEventDidChangeDependencies: () => testStack.push(
+              'did change dependencies 1a',
+            ),
+            stateEventDispose: () => testStack.push('dispose 1a'),
+          ),
+        ],
+        parentContext: RT_TestBed.rootContext,
+      );
+
+      await Future.delayed(Duration.zero, () {
+        app.framework.updateChildren(
+          widgets: [
+            RT_StatefulTestWidget(
+              key: GlobalKey('new'),
+              stateEventCreateState: () => testStack.push('create state 1b'),
+              stateEventInitState: () => testStack.push('init state 1b'),
+              stateEventBuild: () => testStack.push('build 1b'),
+              stateEventDidUpdateWidget: () => testStack.push(
+                'did update widget 1b',
+              ),
+              stateEventDidChangeDependencies: () => testStack.push(
+                'did change dependencies 1b',
+              ),
+              stateEventDispose: () => testStack.push('dispose 1b'),
+            ),
+          ],
+          updateType: UpdateType.undefined,
+          parentContext: RT_TestBed.rootContext,
+        );
+      });
+
+      await Future.delayed(Duration.zero, () {
+        print(testStack.entries.toString());
+
+        expect(testStack.popFromStart(), equals('create state 1a'));
+        expect(testStack.popFromStart(), equals('init state 1a'));
+        expect(
+          testStack.popFromStart(),
+          equals('did change dependencies 1a'),
+        );
+        expect(testStack.popFromStart(), equals('build 1a'));
+        expect(testStack.popFromStart(), equals('dispose 1a'));
+        // after dispose
+        expect(testStack.popFromStart(), equals('create state 1b'));
+        expect(testStack.popFromStart(), equals('init state 1b'));
+        expect(
+          testStack.popFromStart(),
+          equals('did change dependencies 1b'),
+        );
+        expect(testStack.popFromStart(), equals('build 1b'));
+        expect(testStack.canPop(), equals(false));
+      });
+    });
+
+    //
+  });
+}
