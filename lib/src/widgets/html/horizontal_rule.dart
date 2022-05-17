c16a7d8bc6a79ee025d90f2e645a3501f89d8500
erlage <erlage.inc@gmail.com>
2022-05-17 21:18:29 +0530

    Add: GlobalAttributes to HTML's HorizontalRule widget

--- lib/src/widgets/html/horizontal_rule.dart
+++ lib/src/widgets/html/horizontal_rule.dart
@@ -1,22 +1,47 @@
 import 'package:meta/meta.dart';
 
 import 'package:rad/src/core/common/enums.dart';
+import 'package:rad/src/core/common/types.dart';
 import 'package:rad/src/widgets/abstract/widget.dart';
 import 'package:rad/src/core/common/objects/key.dart';
-import 'package:rad/src/widgets/utils/common_props.dart';
-import 'package:rad/src/core/common/objects/render_object.dart';
-import 'package:rad/src/core/common/objects/build_context.dart';
-import 'package:rad/src/core/common/objects/element_description.dart';
+import 'package:rad/src/widgets/abstract/markup_tag_with_global_props.dart';
 
 /// The HorizontalRule widget (HTML's `hr` tag).
 ///
-class HorizontalRule extends Widget {
-  final String? classAttribute;
-
+class HorizontalRule extends MarkUpTagWithGlobalProps {
   const HorizontalRule({
     Key? key,
-    this.classAttribute,
-  }) : super(key: key);
+    String? id,
+    String? title,
+    String? style,
+    String? classAttribute,
+    int? tabIndex,
+    bool? draggable,
+    bool? contenteditable,
+    Map<String, String>? dataAttributes,
+    bool? hidden,
+    String? onClick,
+    EventCallback? onClickEventListener,
+    String? innerText,
+    Widget? child,
+    List<Widget>? children,
+  }) : super(
+          key: key,
+          id: id,
+          title: title,
+          style: style,
+          classAttribute: classAttribute,
+          tabIndex: tabIndex,
+          draggable: draggable,
+          contenteditable: contenteditable,
+          dataAttributes: dataAttributes,
+          hidden: hidden,
+          onClick: onClick,
+          onClickEventListener: onClickEventListener,
+          innerText: innerText,
+          child: child,
+          children: children,
+        );
 
   @nonVirtual
   @override
@@ -24,65 +49,4 @@ class HorizontalRule extends Widget {
 
   @override
   get correspondingTag => DomTag.horizontalRule;
-
-  @override
-  createConfiguration() => _HorizontalRuleConfiguration(classAttribute);
-
-  @override
-  isConfigurationChanged(
-    covariant _HorizontalRuleConfiguration oldConfiguration,
-  ) {
-    return classAttribute != oldConfiguration.classAttribute;
-  }
-
-  @override
-  createRenderObject(context) => _HorizontalRuleRenderObject(context);
-}
-
-/*
-|--------------------------------------------------------------------------
-| configuration
-|--------------------------------------------------------------------------
-*/
-
-class _HorizontalRuleConfiguration extends WidgetConfiguration {
-  final String? classAttribute;
-
-  const _HorizontalRuleConfiguration(this.classAttribute);
-}
-
-/*
-|--------------------------------------------------------------------------
-| render object
-|--------------------------------------------------------------------------
-*/
-
-class _HorizontalRuleRenderObject extends RenderObject {
-  const _HorizontalRuleRenderObject(BuildContext context) : super(context);
-
-  @override
-  render({
-    required covariant _HorizontalRuleConfiguration configuration,
-  }) {
-    return ElementDescription(
-      classes: CommonProps.prepareClasses(
-        classAttribute: configuration.classAttribute,
-        oldClassAttribute: null,
-      ),
-    );
-  }
-
-  @override
-  update({
-    required updateType,
-    required covariant _HorizontalRuleConfiguration oldConfiguration,
-    required covariant _HorizontalRuleConfiguration newConfiguration,
-  }) {
-    return ElementDescription(
-      classes: CommonProps.prepareClasses(
-        classAttribute: newConfiguration.classAttribute,
-        oldClassAttribute: oldConfiguration.classAttribute,
-      ),
-    );
-  }
 }
