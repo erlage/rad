4d2d375664f20435f654778a1f5cd6b3f1ab58a2
erlage <erlage.inc@gmail.com>
2022-05-17 21:18:11 +0530

    Add: GlobalAttributes to HTML's BreakLine widget

--- lib/src/widgets/html/break_line.dart
+++ lib/src/widgets/html/break_line.dart
@@ -1,15 +1,47 @@
 import 'package:meta/meta.dart';
 
 import 'package:rad/src/core/common/enums.dart';
+import 'package:rad/src/core/common/types.dart';
 import 'package:rad/src/widgets/abstract/widget.dart';
 import 'package:rad/src/core/common/objects/key.dart';
-import 'package:rad/src/core/common/objects/build_context.dart';
-import 'package:rad/src/core/common/objects/render_object.dart';
+import 'package:rad/src/widgets/abstract/markup_tag_with_global_props.dart';
 
 /// The BreakLine widget (HTML's `br` tag).
 ///
-class BreakLine extends Widget {
-  const BreakLine({Key? key}) : super(key: key);
+class BreakLine extends MarkUpTagWithGlobalProps {
+  const BreakLine({
+    Key? key,
+    String? id,
+    String? title,
+    String? style,
+    String? classAttribute,
+    int? tabIndex,
+    bool? draggable,
+    bool? contenteditable,
+    Map<String, String>? dataAttributes,
+    bool? hidden,
+    String? onClick,
+    EventCallback? onClickEventListener,
+    String? innerText,
+    Widget? child,
+    List<Widget>? children,
+  }) : super(
+          key: key,
+          id: id,
+          title: title,
+          style: style,
+          classAttribute: classAttribute,
+          tabIndex: tabIndex,
+          draggable: draggable,
+          contenteditable: contenteditable,
+          dataAttributes: dataAttributes,
+          hidden: hidden,
+          onClick: onClick,
+          onClickEventListener: onClickEventListener,
+          innerText: innerText,
+          child: child,
+          children: children,
+        );
 
   @nonVirtual
   @override
@@ -17,23 +49,4 @@ class BreakLine extends Widget {
 
   @override
   get correspondingTag => DomTag.breakLine;
-
-  @override
-  createConfiguration() => const WidgetConfiguration();
-
-  @override
-  isConfigurationChanged(oldConfiguration) => false;
-
-  @override
-  createRenderObject(context) => _BreakLineRenderObject(context);
-}
-
-/*
-|--------------------------------------------------------------------------
-| render object
-|--------------------------------------------------------------------------
-*/
-
-class _BreakLineRenderObject extends RenderObject {
-  const _BreakLineRenderObject(BuildContext context) : super(context);
 }
