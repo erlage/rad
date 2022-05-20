565d67d6a5e460fe96c998abcfadebe31d492021
erlage <erlage.inc@gmail.com>
2022-05-20 14:24:39 +0530

    Public API: Sort imports

--- lib/widgets_async.dart
+++ lib/widgets_async.dart
@@ -4,11 +4,11 @@
 ///
 library widgets_async;
 
-export 'src/include/async/connection_state.dart' show ConnectionState;
 export 'src/include/async/async_snapshot.dart' show AsyncSnapshot;
+export 'src/include/async/connection_state.dart' show ConnectionState;
+export 'src/include/foundation/change_notifier.dart' show ChangeNotifier;
+export 'src/include/foundation/change_notifier.dart' show ValueListenable;
+export 'src/include/foundation/change_notifier.dart' show ValueNotifier;
 export 'src/include/widgets/future_builder.dart' show FutureBuilder;
 export 'src/include/widgets/stream_builder.dart' show StreamBuilder;
-export 'src/include/foundation/change_notifier.dart' show ChangeNotifier;
-export 'src/include/foundation/change_notifier.dart' show ValueNotifier;
-export 'src/include/foundation/change_notifier.dart' show ValueListenable;
 export 'src/include/widgets/value_listenable_builder.dart';
