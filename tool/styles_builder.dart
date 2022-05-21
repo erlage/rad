1faa634c1d149f5098f0f7e66bb17b6b39da80c2
erlage <erlage.inc@gmail.com>
2022-05-21 18:40:04 +0530

    Update: Styles Builder - Clean linter warnings

--- tool/styles_builder.dart
+++ tool/styles_builder.dart
@@ -143,6 +143,9 @@ class StylesBuilder implements Builder {
     }
 
     genContents = '// ignore_for_file: non_constant_identifier_names\n'
+        '// ignore_for_file: directives_ordering\n'
+        '// ignore_for_file: prefer_single_quotes\n'
+        '// ignore_for_file: avoid_escaping_inner_quotes\n'
         "\n// auto-generated. please don't edit this file\n\n"
         '$importStatements'
         'final GEN_STYLES_${genConstant}_CSS = ""$genContents';
