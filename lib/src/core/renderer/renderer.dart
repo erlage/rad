erlage/rad

commit a4ce4c32f602430c3ab4967fee2cbe02c0cd407c

Renderer: Use just updateWidgetsUnderContext on child nodes.

lib/src/core/renderer/renderer.dart
@@ -636,36 +636,17 @@ class Renderer with ServicesResolver {
 
       /*
       |------------------------------------------------------------------------
-      | check whether widget childs has to be updated
+      | run update on child nodes
       |------------------------------------------------------------------------
       */
 
-      // whether old widget happen to have child widgets
-      var hadChilds = oldWidget.widgetChildren.isNotEmpty;
-
-      // whether new widget has any childs
-      var hasChilds = updateObject.widget.widgetChildren.isNotEmpty;
-
-      // if widget has childs, update them
-      if (hasChilds) {
-        updateWidgetsUnderContext(
-          jobQueue: jobQueue,
-          updateType: updateType,
-          parentContext: widgetObject.context,
-          flagAddIfNotFound: flagAddIfNotFound,
-          widgets: updateObject.widget.widgetChildren,
-        );
-      } else {
-        // if new widget has no childs but old had, we have to remove
-        // those orphan childs.
-        if (hadChilds) {
-          disposeWidgets(
-            jobQueue: jobQueue,
-            flagPreserveTarget: true,
-            context: widgetObject.context,
-          );
-        }
-      }
+      updateWidgetsUnderContext(
+        jobQueue: jobQueue,
+        updateType: updateType,
+        parentContext: widgetObject.context,
+        flagAddIfNotFound: flagAddIfNotFound,
+        widgets: updateObject.widget.widgetChildren,
+      );
     } else {
       services.debug.exception(Constants.coreError);
     }

commit 36f96f2a11f379d981971b45deddd2fa58171fc5

Renderer: Fix framework failing to distinguise between direct/non-direct childs - Fix bug that was introduced in a4ce4c32f602430c3ab4967fee2cbe02c0cd407c

lib/src/core/renderer/renderer.dart
@@ -670,17 +670,35 @@ class Renderer with ServicesResolver {
       /*
       |------------------------------------------------------------------------
       | run update on child nodes
+      |
+      | there are two types of child a widget can have:
+      |
+      | - direct(provided in widget's widgetChildren getter)
+      | - non-direct(rendered by the widget's state)
+      |
+      | we run updateWidgetsUnderContext() only if there are direct childs 
+      | because framework is not responsible for dispatching updates to 
+      | non-direct childs of a widget.
       |------------------------------------------------------------------------
       */
 
-      updateWidgetsUnderContext(
-        jobQueue: jobQueue,
-        updateType: updateType,
-        parentContext: widgetObject.context,
-        parentObject: widgetObject,
-        flagAddIfNotFound: flagAddIfNotFound,
-        widgets: updateObject.widget.widgetChildren,
-      );
+      // whether old widget happen to have direct child widgets
+      var hadChilds = oldWidget.widgetChildren.isNotEmpty;
+
+      // whether new widget has direct childs
+      var hasChilds = updateObject.widget.widgetChildren.isNotEmpty;
+
+      // if widget has or had direct childs, run update
+      if (hasChilds || hadChilds) {
+        updateWidgetsUnderContext(
+          jobQueue: jobQueue,
+          updateType: updateType,
+          parentContext: widgetObject.context,
+          parentObject: widgetObject,
+          flagAddIfNotFound: flagAddIfNotFound,
+          widgets: updateObject.widget.widgetChildren,
+        );
+      }
     } else {
       services.debug.exception(Constants.coreError);
     }