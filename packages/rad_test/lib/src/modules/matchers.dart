ad5db567b9d1c63fbefbdca8da2eca7da635d7f7
erlage <erlage.inc@gmail.com>
2022-06-07 18:02:53 +0530

    Add hasContents matcher for matching contents of dom node

--- packages/rad_test/lib/src/modules/matchers.dart
+++ packages/rad_test/lib/src/modules/matchers.dart
@@ -99,6 +99,10 @@ const Matcher hasOneLineDescription = _HasOneLineDescription();
 ///
 const Matcher hasAGoodToStringDeep = _HasGoodToStringDeep();
 
+/// Asserts that textual contents of a dom's node are equals to [contents].
+///
+Matcher hasContents(String contents) => _DomNodeHasContents(contents);
+
 /// A matcher for [AssertionError].
 ///
 /// This is equivalent to `isInstanceOf<AssertionError>()`.
@@ -381,5 +385,55 @@ class _HasGoodToStringDeep extends Matcher {
   @override
   Description describe(Description description) {
     return description.add('multi line description');
+  }
+}
+
+class _DomNodeHasContents extends Matcher {
+  final String expected;
+
+  const _DomNodeHasContents(this.expected);
+
+  @override
+  matches(Object? item, void _) => _domNodeText(item) == expected;
+
+  @override
+  describe(description) {
+    description.add(expected);
+
+    return description;
+  }
+
+  @override
+  describeMismatch(
+    item,
+    mismatchDescription,
+    void _,
+    void __,
+  ) {
+    mismatchDescription.add("Serialized contents: '${_domNodeText(item)}'");
+
+    return mismatchDescription;
+  }
+}
+
+String? _domNodeText(Object? n) {
+  if (n is Iterable) {
+    return n.map(_domNodeText).join('|');
+  } else if (n is Node) {
+    if (n is ContentElement) {
+      return _domNodeText(n.getDistributedNodes());
+    }
+
+    if (n is Element && n.shadowRoot != null) {
+      return _domNodeText(n.shadowRoot!.nodes);
+    }
+
+    if (n.nodes.isNotEmpty) {
+      return _domNodeText(n.nodes);
+    }
+
+    return n.text;
+  } else {
+    return '$n';
   }
 }
