aa4fd604422da6fcb14f1ea85a8e24f6374304a3
erlage <erlage.inc@gmail.com>
2022-07-28 10:27:18 +0530

    Add test templates for value property

--- packages/rad/test/scripts/html_tests.py
+++ packages/rad/test/scripts/html_tests.py
@@ -307,7 +307,7 @@ widget_specific_tests = {
         'html_attr_on_key_press',
         'html_attr_on_key_up',
         'html_attr_on_key_down',
-        'html_prop_value',
+        'html_prop_value_widget_class',
     ],
     'Time': [
         'html_attr_datetime',
