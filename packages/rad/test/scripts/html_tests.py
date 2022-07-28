093dda22e8cc2b169e130926b6b31ef2152f5a20
erlage <erlage.inc@gmail.com>
2022-07-28 11:38:41 +0530

    InputSearch: Use value-property instead of value-attribute

--- packages/rad/test/scripts/html_tests.py
+++ packages/rad/test/scripts/html_tests.py
@@ -572,7 +572,7 @@ widget_specific_tests = {
         'html_attr_form',
         'html_attr_input_mode',
         'html_attr_tabindex',
-        'html_attr_value', 
+        'html_prop_value', 
     ],
     'InputSubmit': [
         'html_attr_form_action',
--- packages/rad/test/tests/generated/html/html_input_search_tests.generated.dart
+++ packages/rad/test/tests/generated/html/html_input_search_tests.generated.dart
@@ -2699,7 +2699,7 @@ void html_input_search_test() {
       expect(domNode3.getAttribute('tabindex'), equals('3'));
     });
 
-    test('should set attribute "value"', () async {
+    test('should set property "value"', () async {
       await app!.buildChildren(
         widgets: [
           InputSearch(key: Key('el-1'), value: 'some-value'),
@@ -2711,11 +2711,11 @@ void html_input_search_test() {
       var domNode1 = app!.domNodeByKeyValue('el-1');
       var domNode2 = app!.domNodeByKeyValue('el-2');
 
-      expect(domNode1.getAttribute('value'), equals('some-value'));
-      expect(domNode2.getAttribute('value'), equals('another-value'));
+      expect((domNode1 as InputElement).value, equals('some-value'));
+      expect((domNode2 as InputElement).value, equals('another-value'));
     });
 
-    test('should update attribute "value"', () async {
+    test('should update property "value"', () async {
       await app!.buildChildren(
         widgets: [
           InputSearch(key: Key('el-1'), value: 'some-value'),
@@ -2736,11 +2736,11 @@ void html_input_search_test() {
       var domNode1 = app!.domNodeByKeyValue('el-1');
       var domNode2 = app!.domNodeByKeyValue('el-2');
 
-      expect(domNode1.getAttribute('value'), equals('updated-value'));
-      expect(domNode2.getAttribute('value'), equals('another-value'));
+      expect((domNode1 as InputElement).value, equals('updated-value'));
+      expect((domNode2 as InputElement).value, equals('another-value'));
     });
 
-    test('should clear attribute "value"', () async {
+    test('should clear property "value"', () async {
       await app!.buildChildren(
         widgets: [
           InputSearch(key: Key('el-1')),
@@ -2761,11 +2761,11 @@ void html_input_search_test() {
       var domNode1 = app!.domNodeByKeyValue('el-1');
       var domNode2 = app!.domNodeByKeyValue('el-2');
 
-      expect(domNode1.getAttribute('value'), equals(null));
-      expect(domNode2.getAttribute('value'), equals(null));
+      expect((domNode1 as InputElement).value, equals(''));
+      expect((domNode2 as InputElement).value, equals(''));
     });
 
-    test('should clear attribute "value" if updated value is null', () async {
+    test('should clear property "value" if updated value is null', () async {
       await app!.buildChildren(
         widgets: [
           InputSearch(key: Key('el-1'), value: 'some-value'),
@@ -2783,11 +2783,10 @@ void html_input_search_test() {
 
       var domNode1 = app!.domNodeByKeyValue('el-1');
 
-      expect(domNode1.getAttribute('value'), equals(null));
+      expect((domNode1 as InputElement).value, equals(''));
     });
 
-    test('should not set attribute "value" if provided value is null',
-        () async {
+    test('should not set property "value" if provided value is null', () async {
       await app!.buildChildren(
         widgets: [
           InputSearch(key: Key('el-1'), value: null),
@@ -2797,7 +2796,7 @@ void html_input_search_test() {
 
       var domNode1 = app!.domNodeByKeyValue('el-1');
 
-      expect(domNode1.getAttribute('value'), equals(null));
+      expect((domNode1 as InputElement).value, equals(''));
     });
   });
 }
