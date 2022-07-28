c833e0526b2bdee74d89f7f889a1ab9151e3a336
erlage <erlage.inc@gmail.com>
2022-07-28 11:25:06 +0530

    InputPassword: Use value-property instead of value-attribute

--- packages/rad/test/scripts/html_tests.py
+++ packages/rad/test/scripts/html_tests.py
@@ -512,7 +512,7 @@ widget_specific_tests = {
         'html_attr_form',
         'html_attr_input_mode',
         'html_attr_tabindex',
-        'html_attr_value', 
+        'html_prop_value', 
     ],
     'InputRadio': [
         'html_attr_checked',
