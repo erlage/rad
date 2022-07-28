326ba9463f1094453ba8f27a76bd6783a1a60204
erlage <erlage.inc@gmail.com>
2022-07-28 23:05:14 +0530

    Add tests for WatchfulRenderElement

---
+++ packages/rad/test/fixtures/test_widget_watchful.dart
@@ -0,0 +1,146 @@
+// Copyright (c) 2022, Rad developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: camel_case_types
+
+import '../test_imports.dart';
+
+/// A watchful widget for testing.
+///
+class RT_WatchfulWidget extends Widget {
+  final VoidCallback? eventInit;
+  final VoidCallback? eventAfterMount;
+  final VoidCallback? eventRender;
+  final VoidCallback? eventUpdate;
+  final VoidCallback? eventAfterUpdate;
+  final VoidCallback? eventDispose;
+  final VoidCallback? eventAfterUnMount;
+
+  final List<Widget>? children;
+
+  const RT_WatchfulWidget({
+    Key? key,
+    this.eventInit,
+    this.eventRender,
+    this.eventUpdate,
+    this.eventAfterMount,
+    this.eventAfterUpdate,
+    this.eventDispose,
+    this.eventAfterUnMount,
+    this.children,
+  }) : super(key: key);
+
+  @override
+  DomTagType? get correspondingTag => null;
+
+  @override
+  createRenderElement(parent) => _WatchfulElement(this, parent);
+
+  @override
+  shouldUpdateWidget(oldWidget) => true;
+}
+
+class _WatchfulElement extends WatchfulRenderElement {
+  _WatchfulElement(
+    RT_WatchfulWidget widget,
+    RenderElement parent,
+  )   :
+        // prepare child widgets
+
+        _children = widget.children ?? const [],
+
+        // base
+
+        super(widget, parent);
+
+  @override
+  List<Widget> get widgetChildren => _children;
+  List<Widget> _children;
+
+  RT_WatchfulWidget get currentWidget => widget as RT_WatchfulWidget;
+
+  @override
+  void init() {
+    super.init();
+
+    if (null != currentWidget.eventInit) {
+      currentWidget.eventInit!();
+    }
+  }
+
+  @override
+  render({required Widget widget}) {
+    var patch = super.render(widget: widget);
+
+    if (null != currentWidget.eventRender) {
+      currentWidget.eventRender!();
+    }
+
+    return patch;
+  }
+
+  @override
+  void afterMount() {
+    super.afterMount();
+
+    if (null != currentWidget.eventAfterMount) {
+      currentWidget.eventAfterMount!();
+    }
+  }
+
+  @override
+  void afterUpdate() {
+    super.afterUpdate();
+
+    if (null != currentWidget.eventAfterUpdate) {
+      currentWidget.eventAfterUpdate!();
+    }
+  }
+
+  @override
+  update({
+    required UpdateType updateType,
+    required Widget oldWidget,
+    required Widget newWidget,
+  }) {
+    var patch = super.update(
+      updateType: updateType,
+      oldWidget: oldWidget,
+      newWidget: newWidget,
+    );
+
+    if (null != currentWidget.eventUpdate) {
+      currentWidget.eventUpdate!();
+    }
+
+    return patch;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    if (null != currentWidget.eventDispose) {
+      currentWidget.eventDispose!();
+    }
+  }
+
+  @override
+  void afterUnMount() {
+    super.afterUnMount();
+
+    if (null != currentWidget.eventAfterUnMount) {
+      currentWidget.eventAfterUnMount!();
+    }
+  }
+
+  @override
+  afterWidgetRebind({
+    required oldWidget,
+    required covariant RT_WatchfulWidget newWidget,
+    required updateType,
+  }) {
+    _children = newWidget.children ?? const [];
+  }
+}
---
+++ packages/rad/test/tests/misc/watchful_render_element_test.dart
@@ -0,0 +1,234 @@
+// Copyright (c) 2022, Rad developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: avoid_relative_lib_imports
+
+import '../../test_imports.dart' hide RootRenderElement;
+
+// these tests are mere to cover unreachable code
+
+void main() {
+  test(
+    'should call init '
+    ', exactly once '
+    ', before render ',
+    () async {
+      var app = createTestApp()..start();
+      var stack = RT_TestStack();
+
+      await app.buildChildren(
+        widgets: [
+          RT_WatchfulWidget(
+            eventInit: () => stack.push('init'),
+            eventRender: () => stack.push('render'),
+          ),
+        ],
+        parentRenderElement: app.appRenderElement,
+      );
+
+      app.stop();
+      await Future.delayed(Duration(milliseconds: 100));
+
+      expect(stack.popFromStart(), equals('init'));
+      expect(stack.popFromStart(), equals('render'));
+      expect(stack.canPop(), equals(false));
+    },
+  );
+
+  test(
+    'should call render '
+    ', exactly once '
+    ', after init '
+    ', before update ',
+    () async {
+      var app = createTestApp()..start();
+      var stack = RT_TestStack();
+
+      await app.buildChildren(
+        widgets: [
+          RT_WatchfulWidget(
+            eventInit: () => stack.push('init'),
+            eventRender: () => stack.push('render'),
+            eventUpdate: () => stack.push('update'),
+          ),
+        ],
+        parentRenderElement: app.appRenderElement,
+      );
+
+      await app.updateChildren(
+        widgets: [
+          RT_WatchfulWidget(
+            eventInit: () => stack.push('init'),
+            eventRender: () => stack.push('render'),
+            eventUpdate: () => stack.push('update'),
+          ),
+        ],
+        updateType: UpdateType.setState,
+        parentRenderElement: app.appRenderElement,
+      );
+
+      app.stop();
+      await Future.delayed(Duration(milliseconds: 100));
+
+      expect(stack.popFromStart(), equals('init'));
+      expect(stack.popFromStart(), equals('render'));
+      expect(stack.popFromStart(), equals('update'));
+      expect(stack.canPop(), equals(false));
+    },
+  );
+
+  test(
+    'should call afterMount '
+    ', exactly once '
+    ', after render ',
+    () async {
+      var app = createTestApp()..start();
+      var stack = RT_TestStack();
+
+      await app.buildChildren(
+        widgets: [
+          RT_WatchfulWidget(
+            children: [Text('hello world')],
+            eventRender: () => stack.push('render'),
+            eventAfterMount: () {
+              stack.push('after mount');
+
+              expect(app.appDomNode, RT_hasContents('hello world'));
+            },
+          ),
+        ],
+        parentRenderElement: app.appRenderElement,
+      );
+
+      app.stop();
+      await Future.delayed(Duration(milliseconds: 100));
+
+      expect(stack.popFromStart(), equals('render'));
+      expect(stack.popFromStart(), equals('after mount'));
+      expect(stack.canPop(), equals(false));
+    },
+  );
+
+  test('should call afterUpdate after update & dom update', () async {
+    var app = createTestApp()..start();
+    var stack = RT_TestStack();
+
+    await app.buildChildren(
+      widgets: [
+        RT_WatchfulWidget(
+          children: [Text('initial contents')],
+          eventUpdate: () => stack.push('update'),
+          eventAfterUpdate: () {
+            stack.push('after update');
+
+            expect(app.appDomNode, RT_hasContents('initial contents'));
+          },
+        ),
+      ],
+      parentRenderElement: app.appRenderElement,
+    );
+
+    await app.updateChildren(
+      widgets: [
+        RT_WatchfulWidget(
+          children: [Text('updated contents')],
+          eventUpdate: () => stack.push('update'),
+          eventAfterUpdate: () {
+            stack.push('after update');
+
+            expect(app.appDomNode, RT_hasContents('updated contents'));
+          },
+        ),
+      ],
+      updateType: UpdateType.setState,
+      parentRenderElement: app.appRenderElement,
+    );
+
+    app.stop();
+    await Future.delayed(Duration(milliseconds: 100));
+
+    expect(stack.popFromStart(), equals('update'));
+    expect(stack.popFromStart(), equals('after update'));
+    expect(stack.canPop(), equals(false));
+  });
+
+  test('should call update after render', () async {
+    var app = createTestApp()..start();
+    var stack = RT_TestStack();
+
+    await app.buildChildren(
+      widgets: [
+        RT_WatchfulWidget(
+          children: [Text('hello world')],
+          eventRender: () => stack.push('render'),
+          eventUpdate: () => stack.push('update'),
+        ),
+      ],
+      parentRenderElement: app.appRenderElement,
+    );
+
+    await app.updateChildren(
+      widgets: [
+        RT_WatchfulWidget(
+          children: [Text('hello world')],
+          eventRender: () => stack.push('render'),
+          eventUpdate: () => stack.push('update'),
+        ),
+      ],
+      updateType: UpdateType.setState,
+      parentRenderElement: app.appRenderElement,
+    );
+
+    app.stop();
+    await Future.delayed(Duration(milliseconds: 100));
+
+    expect(stack.popFromStart(), equals('render'));
+    expect(stack.popFromStart(), equals('update'));
+    expect(stack.canPop(), equals(false));
+  });
+
+  test('should call dispose before actual unmount', () async {
+    var stack = RT_TestStack();
+
+    var runner = runApp(
+      app: RT_WatchfulWidget(
+        children: [Text('hello world')],
+        eventDispose: () {
+          stack.push('dispose');
+          expect(RT_TestBed.rootDomNode, RT_hasContents('hello world'));
+        },
+      ),
+      appTargetId: RT_TestBed.rootTargetId,
+    );
+
+    await Future.delayed(Duration(milliseconds: 100));
+    runner.stop();
+    await Future.delayed(Duration(milliseconds: 100));
+
+    expect(stack.popFromStart(), equals('dispose'));
+    expect(stack.canPop(), equals(false));
+  });
+
+  test('should call afterUnMount after actual unmount', () async {
+    var stack = RT_TestStack();
+
+    var runner = runApp(
+      app: RT_WatchfulWidget(
+        children: [Text('hello world')],
+        eventAfterUnMount: () {
+          stack.push('after unmount');
+          expect(RT_TestBed.rootDomNode, RT_hasContents(''));
+        },
+      ),
+      appTargetId: RT_TestBed.rootTargetId,
+    );
+
+    await Future.delayed(Duration(milliseconds: 100));
+    runner.stop();
+    await Future.delayed(Duration(milliseconds: 100));
+
+    expect(stack.popFromStart(), equals('after unmount'));
+    expect(stack.canPop(), equals(false));
+  });
+}
