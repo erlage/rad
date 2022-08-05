a7af02af51b5c2b6fbd26655b9b5a891621489de
erlage <erlage.inc@gmail.com>
2022-08-05 11:17:46 +0530

    Add tests for callback refs

--- packages/rad/test/tests/generated/html/html_image_map_tests.generated.dart
+++ packages/rad/test/tests/generated/html/html_image_map_tests.generated.dart
@@ -524,6 +524,100 @@ void html_image_map_test() {
       expect(domNode3.getAttribute('title'), equals("some 'messy' title"));
     });
 
+    test('should call ref with correct element', () async {
+      Element? el1;
+      Element? el2;
+      Element? el3;
+
+      await app!.buildChildren(
+        widgets: [
+          ImageMap(id: 'some-id-1', ref: (el) => el1 = el),
+          ImageMap(id: 'some-id-2', ref: (el) => el2 = el),
+          ImageMap(id: 'some-id-3', ref: (el) => el3 = el),
+        ],
+        parentRenderElement: app!.appRenderElement,
+      );
+
+      expect(el1, equals(document.getElementById('some-id-1')));
+      expect(el2, equals(document.getElementById('some-id-2')));
+      expect(el3, equals(document.getElementById('some-id-3')));
+    });
+
+    test('should call ref-callback before afterMounts', () async {
+      Element? el1;
+      var stack = RT_TestStack();
+
+      await app!.buildChildren(
+        widgets: [
+          RT_TestWidget(roEventAfterMount: () {
+            stack.push('after mount');
+            expect(el1, equals(document.getElementById('some-id-1')));
+          }),
+          ImageMap(id: 'some-id-1', ref: (el) => el1 = el),
+        ],
+        parentRenderElement: app!.appRenderElement,
+      );
+
+      expect(stack.popFromStart(), equals('after mount'));
+      expect(stack.canPop(), equals(false));
+    });
+
+    test('should call ref-callback with null on dispose', () async {
+      Element? el1;
+      var stack = RT_TestStack();
+
+      await app!.buildChildren(
+        widgets: [
+          RT_TestWidget(
+            roEventDispose: () {
+              stack.push('dispose');
+              expect(el1, equals(null));
+            },
+            // child widgets are disposed first
+            children: [
+              ImageMap(id: 'some-id-1', ref: (el) => el1 = el),
+            ],
+          ),
+        ],
+        parentRenderElement: app!.appRenderElement,
+      );
+
+      await app!.updateChildren(
+        widgets: [ImageMap()],
+        updateType: UpdateType.setState,
+        parentRenderElement: app!.appRenderElement,
+      );
+
+      expect(stack.popFromStart(), equals('dispose'));
+      expect(stack.canPop(), equals(false));
+    });
+
+    test('should not call ref-callback on update', () async {
+      var stack = RT_TestStack();
+
+      await app!.buildChildren(
+        widgets: [
+          ImageMap(ref: (el) {
+            stack.push('callback');
+          }),
+        ],
+        parentRenderElement: app!.appRenderElement,
+      );
+
+      await app!.updateChildren(
+        widgets: [
+          ImageMap(ref: (el) {
+            stack.push('callback');
+          }),
+        ],
+        updateType: UpdateType.setState,
+        parentRenderElement: app!.appRenderElement,
+      );
+
+      expect(stack.popFromStart(), equals('callback'));
+      expect(stack.canPop(), equals(false));
+    });
+
     test('should set correct types and markup', () async {
       await app!.buildChildren(
         widgets: [
