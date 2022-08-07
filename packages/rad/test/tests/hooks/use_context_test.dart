048b6bcf7af54902182ebfc6a069953ac5c38b03
erlage <erlage.inc@gmail.com>
2022-08-07 16:36:52 +0530

    Add useContext hook

---
+++ packages/rad/test/tests/hooks/use_context_test.dart
@@ -0,0 +1,66 @@
+// Copyright (c) 2022, Rad developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '../../test_imports.dart';
+
+void main() {
+  RT_AppRunner? app;
+
+  setUp(() {
+    app = createTestApp()..start();
+  });
+
+  tearDown(() => app!.stop());
+
+  test('should return context of scope', () async {
+    BuildContext? context;
+
+    await app!.buildChildren(
+      widgets: [
+        HookScope(
+          () {
+            context = useContext();
+
+            return Text('hello world');
+          },
+          key: Key('scope'),
+        ),
+      ],
+      parentRenderElement: app!.appRenderElement,
+    );
+
+    var renderElement = app!.renderElementByKeyValue('scope') as BuildContext;
+    expect(renderElement, equals(context));
+  });
+
+  test('should return context of nearest scope', () async {
+    BuildContext? parent;
+    BuildContext? child;
+
+    await app!.buildChildren(
+      widgets: [
+        HookScope(
+          () {
+            parent = useContext();
+
+            return HookScope(
+              () {
+                child = useContext();
+                return Text('hello world');
+              },
+              key: Key('child'),
+            );
+          },
+          key: Key('parent'),
+        ),
+      ],
+      parentRenderElement: app!.appRenderElement,
+    );
+
+    var childContext = app!.renderElementByKeyValue('child') as BuildContext;
+    var parentContext = app!.renderElementByKeyValue('parent') as BuildContext;
+    expect(childContext, equals(child));
+    expect(parentContext, equals(parent));
+  });
+}
