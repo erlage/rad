24eded50ebab894cb4a0062b8de78b52fc92bba7
erlage <erlage.inc@gmail.com>
2022-08-07 16:34:03 +0530

    Add Scope interface and dispatch unit

---
+++ packages/rad/lib/src/core/interface/scope/abstract.dart
@@ -0,0 +1,29 @@
+// Copyright (c) 2022, Rad developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:meta/meta.dart';
+
+import 'package:rad/src/core/common/abstract/build_context.dart';
+import 'package:rad/src/core/common/enums.dart';
+import 'package:rad/src/core/common/types.dart';
+
+/// A scope interface.
+///
+@internal
+abstract class Scope {
+  /// Nearest BuildContext.
+  ///
+  BuildContext get context;
+
+  /// Rebuild scope.
+  ///
+  void performRebuild();
+
+  /// Add a scope event listener.
+  ///
+  void addScopeEventListener(
+    ScopeEventType eventType,
+    ScopeEventCallback listener,
+  );
+}
