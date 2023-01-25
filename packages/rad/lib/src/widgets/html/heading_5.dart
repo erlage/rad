cf50273dd7039965117dfd9d27af642ed4d9beee
erlage <erlage.inc@gmail.com>
2023-01-25 18:39:46 +0530

    Split Heading tags into separate files

---
+++ packages/rad/lib/src/widgets/html/heading_5.dart
@@ -0,0 +1,44 @@
+// Copyright (c) 2022, Rad developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:rad/src/core/common/enums.dart';
+import 'package:rad/src/core/common/objects/key.dart';
+import 'package:rad/src/core/common/types.dart';
+import 'package:rad/src/widgets/abstract/html_widget_base.dart';
+import 'package:rad/src/widgets/abstract/widget.dart';
+
+/// The Heading5 widget (HTML's `h5` tag).
+///
+class Heading5 extends HTMLWidgetBase {
+  const Heading5({
+    Key? key,
+    NullableElementCallback? ref,
+    String? id,
+    String? title,
+    String? style,
+    String? className,
+    bool? hidden,
+    String? innerText,
+    Widget? child,
+    List<Widget>? children,
+    EventCallback? onClick,
+    Map<String, String>? additionalAttributes,
+  }) : super(
+          key: key,
+          ref: ref,
+          id: id,
+          title: title,
+          style: style,
+          className: className,
+          hidden: hidden,
+          innerText: innerText,
+          child: child,
+          children: children,
+          onClick: onClick,
+          additionalAttributes: additionalAttributes,
+        );
+
+  @override
+  DomTagType get correspondingTag => DomTagType.heading5;
+}
