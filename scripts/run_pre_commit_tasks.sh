adc563a0c8c9ec5f1a06a6b80cdf4373087e7037
erlage <erlage.inc@gmail.com>
2023-06-04 12:18:12 +0530

    Rad HTML VSCode: Add import-sort to pre commit script

--- scripts/run_pre_commit_tasks.sh
+++ scripts/run_pre_commit_tasks.sh
@@ -15,4 +15,8 @@ cd ../rad_hooks
 dart pub run import_sorter:main
 dart format $(find . -name "*.dart" -not -path "*/templates/*" -not -path '*/.*')
 
+cd ../rad_html_vscode
+dart pub run import_sorter:main
+dart format $(find . -name "*.dart" -not -path "*/templates/*" -not -path '*/.*')
+
 cd ../../
