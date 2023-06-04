8fa171a668da0c00d79cbb935d05f11dc5ecd6f1
erlage <erlage.inc@gmail.com>
2023-06-04 12:11:59 +0530

    Update run-tests script to generate & run all tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,12 +1,19 @@
 # this script run tests
 
-cd packages/rad
+# generate tests
+
+cd packages/rad/test && python3 scripts/main.py gen && cd ../../../
+cd packages/rad_html_vscode && python3 test/gen.py
+
+# run all tests
 
 cd ../rad
 dart test
 cd ../rad_test
 dart test
 cd ../rad_hooks
+dart test
+cd ../rad_html_vscode
 dart test
 
 cd ../../
